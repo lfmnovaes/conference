9567292bc0ed39d9cb309e882cbd6817e6d27d1f
Luis <lfmnovaes@gmail.com>
2021-09-08 04:11:33 -0300

    Initial speakers changes
    * Add speakers imgs
    * Add object with speakers info
    * Add style prototype to 1st speaker
    * Remove local css/js bootstrap
    * Fix most of lint errors

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+const speakersObj = [
+  {
+    imgName: 'speaker1.jpg',
+    name: 'Yochai Benkler',
+    title: 'Berkman Professor of Enterpreneurial Legal Studies at Harvard Law School',
+    desc: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006',
+  },
+  {
+    imgName: 'speaker2.jpg',
+    name: 'Kilnam Chon',
+    title: '',
+    desc: 'Kilnam Chon helped bring the internet to Asia and is an outspoken advocate for the open web and digital commons. In 2012, he was inducted into the inaugural class of the Internet Society (ISOC) Internet Hall of Fame.',
+  },
+  {
+    imgName: 'speaker3.jpg',
+    name: 'SohYeong Noh',
+    title: 'Director of Art Centre Nabi and a board member of CC Korea',
+    desc: 'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
+  },
+  {
+    imgName: 'speaker4.jpg',
+    name: 'Julia Leda',
+    title: 'President of Young Pirates of Europe',
+    desc: 'European ingetration, political democracy and participation of youth through online as her major condern, Redas report outlining potential changes to EU copyright law was approved by the Parliament in July.',
+  },
+  {
+    imgName: 'speaker5.jpg',
+    name: 'Lila Tretikov',
+    title: 'Executive Director of the Wikimedia Foundation',
+    desc: 'Lila Tretikov is the Executive Director of the Wikimedia Foundation, the nonprofit organization that operates Wikipedia. Wikipedia is freely available in 290 languages and used by nearly half a billion people around the world every month.',
+  },
+  {
+    imgName: 'speaker6.jpg',
+    name: 'Ryan Merkley',
+    title: 'CEO of Creative Commons, ex COO of the Mozilla Foundation',
+    desc: 'Ryan had been leading open-source projects at the Mozilla Foundation such as the open source movement.',
+  },
+];
